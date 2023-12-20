b35c9c1f854be7e0f5e136bc77d5376596f1f3d6
MirImaadAli1 <hydrogencyanide115@gmail.com>
2023-12-20 19:41:18 +0400

    signup updated and css added

--- src/pages/ProfileDetails.js
+++ src/pages/ProfileDetails.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import './ProfileDetails.css';
-
+import { useRef } from "react";
 
 function ProfileDetails() {
+
+    const imageRef = useRef(null);
+    const [image, setImage] = useState("");
     const [username, setUsername] = useState('');
     const [profileDescription, setProfileDescription] = useState('');
 
@@ -16,42 +19,106 @@ function ProfileDetails() {
         setProfileDescription(event.target.value);
     };
 
+    const handleImageClick = () => {
+        imageRef.current.click();
+    };
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        setImage(event.target.files[0]);
+
+    }
+
     return (
         <div className="proDetBg">
-            <div>
-                {/* Username input box */}
-                <div className="input-boxes">
-                    <div>
-                        <label className="username-label" htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            value={username}
-                            onChange={handleUsernameChange}
-                            placeholder="Enter your desired username"
-                        />
-                    </div>
 
-                    {/* Profile description input box */}
-                    <div>
-                        <label className="profiledesc-label" htmlFor="profileDescription">Profile Description:</label>
-                        <input
-                            type="text"
-                            id="profileDescription"
-                            value={profileDescription}
-                            onChange={handleProfileDescriptionChange}
-                            placeholder="A short description about yourself!"
-                        />
+            {/* Display the entered content below each other */}
+            <div className="results">
+                <div className="image-upload-container">
+                    <div className="box-decoration">
+
+                        <div onClick={handleImageClick} style={{ cursor: "pointer" }}>
+                            {image ? (
+                                <img src={URL.createObjectURL(image)} alt="upload image" className="img-display-after" />
+                            ) : (
+                                <img src="./Ellipse 13.png" alt="upload image" className="img-display-before" />
+                            )}
+
+                            <input
+                                id="image-upload-input"
+                                type="file"
+                                onChange={handleImageChange}
+                                ref={imageRef}
+                                style={{ display: "none" }}
+                            />
+                        </div>
+                        <button onClick={handleImageClick} className="upload-pic-button">
+                            Upload New Photo
+                        </button>
+
+                        {/* <button
+                            className="image-upload-button"
+                            onClick={handleUploadButtonClick}
+                        >
+                            Upload
+                        </button> */}
                     </div>
                 </div>
-
-                {/* Display the entered content below each other */}
-                <div className="results">
-                    <p className="realtime-text">Username: @{username}</p>
+                <div>
+                    <p className="realtime-text">@{username}</p>
                     <p className="realtime-text">Profile Description: {profileDescription}</p>
                 </div>
             </div>
+
+            {/* Username input box */}
+            <div className="input-boxes">
+                <div className="username">
+                    <label className="username-label" htmlFor="username">Username:</label>
+                    <input
+                        className="username-inputbox"
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        placeholder="Enter your desired username"
+                    />
+                </div>
+
+
+
+                {/* Profile description input box */}
+                <div className="desc">
+                    <label className="profiledesc-label" htmlFor="profileDescription">Profile Description:</label>
+                    <input
+                        className="desc-inputbox"
+                        type="text"
+                        id="profileDescription"
+                        value={profileDescription}
+                        onChange={handleProfileDescriptionChange}
+                        placeholder="A short description about yourself!"
+                    />
+                </div>
+                <div className="interests">
+                    Interests:
+                </div>
+                <div className="interest-choice">
+                    <div> Art </div>
+                    <div> Music </div>
+                    <div> Technology </div>
+                    <div> TV/Movies </div>
+                    <div> Dance </div>
+                    <div> Gaming </div>
+                    <div> Sports </div>
+                    <div> Cooking/Baking </div>
+                </div>
+            </div>
+         
+
+
+
+
         </div>
+        
     );
 }
 
