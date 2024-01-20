bda94939379e3a988232e28ff764f261a8e96c04
MomentumAdonis <114130721+MomentumAdonis@users.noreply.github.com>
2024-01-20 19:35:31 +0400

    Syncfusion components

---
+++ src/components/FileExplorer.jsx
@@ -0,0 +1,63 @@
+import {
+  DetailsView,
+  FileManagerComponent,
+  Inject,
+  NavigationPane,
+} from "@syncfusion/ej2-react-filemanager";
+import * as React from "react";
+import "../App.css";
+import { Toolbar } from "@syncfusion/ej2-navigations";
+
+function FileExplorer() {
+  let hostUrl = "https://ej2-aspcore-service.azurewebsites.net/";
+
+  return (
+    <div className="control-section">
+      <FileManagerComponent
+        id="overview_file"
+        ajaxSettings={{
+          url: hostUrl + "api/FileManager/FileOperations",
+          getImageUrl: hostUrl + "api/FileManager/GetImage",
+          uploadUrl: hostUrl + "api/FileManager/Upload",
+          downloadUrl: hostUrl + "api/FileManager/Download",
+        }}
+        toolbarSettings={{
+          items: [
+            "NewFolder",
+            "SortBy",
+            "Cut",
+            "Copy",
+            "Paste",
+            "Delete",
+            "Refresh",
+            "Download",
+            "Rename",
+            "Selection",
+            "View",
+            "Details",
+          ],
+        }}
+        contextMenuSettings={{
+          layout: [
+            "SortBy",
+            "View",
+            "Refresh",
+            "|",
+            "Paste",
+            "|",
+            "NewFolder",
+            "|",
+            "Details",
+            "|",
+            "SelectAll",
+          ],
+        }}
+        view={"Details"}
+      >
+        <Inject services={[NavigationPane, DetailsView, Toolbar]} />
+      </FileManagerComponent>
+    </div>
+  );
+}
+
+export default FileExplorer;
