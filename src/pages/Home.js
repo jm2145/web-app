31feff663b6edf80f1c5542824e4d6b5b2a1a71c
MirImaadAli1 <hydrogencyanide115@gmail.com>
2024-01-29 20:43:39 +0400

    landing page begun

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+// src/components/LandingPage.js
+import React, { useState } from 'react';
+import './Home.css';
 
-function Home(){
+const Home = () => {
+  const [illuminated, setIlluminated] = useState(false);
+
+  const toggleIllumination = () => {
+    setIlluminated((prev) => !prev);
+  };
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <div className='home-bg'>
+      <div className={`moon ${illuminated ? 'illuminated' : ''}`}>
+        <div className="home-text-container">
+          <h1>{illuminated ? 'Illuminated Text' : 'Hidden Text'}</h1>
+        </div>
+      </div>
+      <button className="home-button" onClick={toggleIllumination}>
+        Toggle Illumination
+      </button>
+    </div>
+    </div>
+  );
+};
 
 export default Home;
